8bf97398ac61de24bd39cd97b957ecd75cb816ac
zohaibulhassan <zohaibulhassan0040@gmail.com>
2025-08-29 03:32:14 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-// index.js
-import React from 'react';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
